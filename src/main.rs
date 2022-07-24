d1a89f1ee96aec5ddf6527105e3dc95fc3e1f153
jole <jonas@jole.xyz>
2022-07-24 17:56:48 +0200

    read url from argv

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
+use argh::FromArgs;
 use gurl::Agent;
+use url::Url;
+
+#[derive(FromArgs)]
+/// gURL
+struct Args {
+    #[argh(positional)]
+    url: Url,
+}
 
 fn main() {
-    let resp = Agent::get("gemini://gemini.circumlunar.space/".try_into().unwrap())
-        .unwrap()
-        .run()
-        .unwrap();
+    let args: Args = argh::from_env();
+
+    let resp = Agent::get(args.url).unwrap().run().unwrap();
 
     eprintln!("status: {}", resp.status);
     eprintln!("meta: {}", resp.meta);
